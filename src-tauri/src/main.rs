b0412abca14a01c53aed18dc03912e7c3ddd0e82
jsxiaosi <1531733886@qq.com>
2024-10-23 21:59:50 +0800

    build(tauri): 📦️ tauri V2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
